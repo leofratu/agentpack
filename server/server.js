ddeafd23a0f1c57a0f35fb5c107f7b5e43d4ced8
leofratu <leofratu@users.noreply.github.com>
2026-05-15 10:45:00 +0200

    docs(server): add user schema commentary

--- server/server.js
+++ server/server.js
@@ -233,3 +233,5 @@ app.listen(PORT, () => {
 // Note: Startup information logged on process.env.PORT.
 
 // Note: CORS origins setup checks local front-end ports.
+
+// Schema: Users table registers email and password hashes.
