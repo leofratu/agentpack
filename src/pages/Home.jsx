02bf10e99c6f240389775486d4f2fe92e675cc30
leofratu <leofratu@users.noreply.github.com>
2026-05-19 17:21:03 +0200

    feat: integrate components on the home landing view

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Hero from '../components/Hero'
+import ValueStrip from '../components/ValueStrip'
+import Features from '../components/Features'
+import HowItWorks from '../components/HowItWorks'
+import FeaturedPacks from '../components/FeaturedPacks'
+import CTABanner from '../components/CTABanner'
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <ValueStrip />
+      <Features />
+      <HowItWorks />
+      <FeaturedPacks />
+      <CTABanner />
+    </>
+  )
+}
+
+export default Home
